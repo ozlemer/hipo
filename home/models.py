f7a1fbfb7653ed4a806b039da8472c324248ed69
Ozlem <eeroozlem@gmail.com>
2018-12-28 01:55:01 +0300

    bug-fixed in home/models.py and templates/home_view.html

--- home/models.py
+++ home/models.py
@@ -4,7 +4,6 @@ from django.conf import settings
 
 class Search(models.Model):
     tag = models.CharField(max_length=300)
-    counter = models.PositiveIntegerField(default=0)
     search_date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
--- home/views.py
+++ home/views.py
@@ -1,31 +1,40 @@
 from django.shortcuts import render
 from home.models import Search
 import requests
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 
 API_KEY = '1ddb7df62cbdc4e07f6ec75ca78e2960'
 API = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key={}&tags={}&format=json&nojsoncallback=1'
 
 # Create your views here.
-#ana sayfa
-#rewuest ana sayfaya istek
 
 def home_view(request):
-    results = None
-
+    pictures = None
+    #picture_list = None
     query = request.GET.get('query')
     if query:
         log, created = Search.objects.get_or_create(tag=query)
         log.save()
         url = API.format(API_KEY, query)
-        results = requests.get(url).json()
+        picture_list = requests.get(url).json()
+        paginator = Paginator(picture_list['photos']['photo'], 4)  # Show 25 contacts per page
+        page = request.GET.get('page')
+        try:
+            pictures = paginator.page(page)
+        except PageNotAnInteger:
+            # If page is not an integer, deliver first page.
+            pictures = paginator.page(1)
+        except EmptyPage:
+            pictures = paginator.page(paginator.num_pages)
+    # If page is out of range (e.g. 9999), deliver last page of results.
 
     recent_searches = Search.objects.order_by('-search_date')[0:20]
 
     if len(recent_searches) == 0:
         recent_searches = None
-
     content = {
-        'results': results,
+        'pictures': pictures,
         'recent_searches': recent_searches,
 
     }
